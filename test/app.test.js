31fdd7d5a97da393f319abf914eb6d7ab3752f25
Thomas Barber <tjbninja@gmail.com>
2020-05-16 14:40:11 -0400

    Updated test to allow for a change to content

--- test/app.test.js
+++ test/app.test.js
@@ -8,8 +8,8 @@ describe('gae_node_request_example', () => {
       request(app).get('/').expect(200, done);
     });
 
-    it('should get Hello World', (done) => {
-      request(app).get('/').expect('Hello, world!', done);
+    it('should get Hello, world - now with continuous deployment!', (done) => {
+      request(app).get('/').expect('Hello, world - now with continuous deployment!', done);
     });
   });
 });
